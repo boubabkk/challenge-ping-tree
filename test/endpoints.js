19b590dcd5292bbdd480ac049c29984ec99a28ff
DESKTOP-U28LREN\Konde <aboubacarkonde09@gmail.com>
2021-07-22 22:22:38 -0400

    rewrite codes according to guidelines

--- test/endpoints.js
+++ test/endpoints.js
@@ -1,25 +1,32 @@
 process.env.NODE_ENV = 'test'
 
 var test = require('ava')
+var map = require('map-limit')
 var servertest = require('servertest')
 var server = require('../lib/server')
-var redis = require('../lib/redis')
-var Target = require('../lib/target')
-const conn = new Target(redis, redis.print)
 
-const targetSample = {
-  url: 'http://example.com',
-  value: 0.5,
-  maxAcceptsPerDay: 10,
-  accept: {
-    geoState: {
-      $in: ['ca', 'ny']
-    },
-    hour: {
-      $in: ['13', '14', '15']
+var targets = [
+  {
+    id: 1,
+    url: 'http://example-1.com',
+    value: 0.50,
+    maxAcceptsPerDay: 10,
+    accept: {
+      geoState: ['ca', 'ny'],
+      hour: [13, 14, 15]
+    }
+  },
+  {
+    id: 2,
+    url: 'http://example-2.com',
+    value: 1.50,
+    maxAcceptsPerDay: 2,
+    accept: {
+      geoState: ['ca'],
+      hour: [14, 15]
     }
   }
-}
+]
 
 test.serial.cb('healthcheck', function (t) {
   var url = '/health'
@@ -32,107 +39,75 @@ test.serial.cb('healthcheck', function (t) {
   })
 })
 
-test.serial.cb('create a target returns OK', function (t) {
-  const url = '/api/targets'
-  conn.deleteAll()
-
-  servertest(server(), url, { encoding: 'json', method: 'POST' }, onCreateResponse)
-    .end(JSON.stringify(targetSample))
-
-  function onCreateResponse (err, res) {
-    t.falsy(err, 'no error')
-    t.deepEqual(res.statusCode, 200, 'correct statusCode')
-    t.deepEqual(res.body.result, 'OK', 'return OK')
+test.serial.cb('add targets', t => {
+  map(targets, 1, addTarget, function (err) {
+    t.falsy(err, 'should not error')
     t.end()
+  })
+
+  function addTarget (target, cb) {
+    var opts = { encoding: 'json', method: 'POST' }
+    var req = servertest(server(), '/api/targets', opts, function (err, res) {
+      t.is(res.statusCode, 201, 'correct statusCode')
+      cb(err)
+    })
+
+    req.end(JSON.stringify(target))
   }
 })
 
-test.serial.cb('index returns all targes', function (t) {
-  const url = '/api/targets'
-  conn.deleteAll()
-  conn.addNew(targetSample)
-
-  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+test.serial.cb('return all targets', t => {
+  servertest(server(), '/api/targets', { encoding: 'json' }, function (err, res) {
     t.falsy(err, 'no error')
     t.is(res.statusCode, 200, 'correct statusCode')
-    t.is(res.body.data.length, 1, 'contains created target')
-    t.deepEqual(res.body.data[0], { ...targetSample, id: 0, accepted: {} }, 'contains target data')
+    t.deepEqual(res.body, targets, 'target should match')
     t.end()
   })
 })
 
-test.serial.cb('get returns the proper target', function (t) {
-  const url = '/api/target/0'
-  conn.deleteAll()
-  conn.addNew(targetSample)
-
-  servertest(server(), url, { encoding: 'json' }, function (err, res) {
-    t.falsy(err, 'no error')
-    t.is(res.statusCode, 200, 'correct statusCode')
-
-    t.deepEqual(res.body.data, { ...targetSample, id: 0, accepted: {} }, 'contains target data')
+test.serial.cb('get individual targets', t => {
+  map(targets, 1, getTarget, function (err) {
+    t.falsy(err, 'should not error')
     t.end()
   })
-})
 
-test.serial.cb('update a target returns OK', function (t) {
-  const url = '/api/target/0'
-  conn.deleteAll()
-  conn.addNew(targetSample)
-  const updatedFields = {
-    url: 'http://new.example.com',
-    maxAcceptsPerDay: 20
-  }
-
-  servertest(server(), url, { encoding: 'json', method: 'POST' }, onUpdateResponse)
-    .end(JSON.stringify(updatedFields))
-
-  function onUpdateResponse (err, res) {
-    t.falsy(err, 'no error')
-    t.is(res.statusCode, 200, 'correct statusCode')
-    t.is(res.body.result, 'OK', 'return OK')
-    t.end()
+  function getTarget (target, cb) {
+    var opts = { encoding: 'json' }
+    servertest(server(), '/api/target/' + target.id, opts, function (err, res) {
+      if (err) return cb(err)
+      t.is(res.statusCode, 200, 'correct statusCode')
+      t.deepEqual(res.body, target, 'target should match')
+      cb()
+    })
   }
 })
 
-test.serial.cb('route returns reject decision', function (t) {
-  const url = '/route'
-  conn.deleteAll()
-  conn.addNew(targetSample)
-  const visit = {
-    geoState: 'ca',
-    publisher: 'abc',
-    timestamp: '2018-07-19T23:28:59.513Z'
-  }
+test.serial.cb('should receive decisions', t => {
+  var visits = [
+    { timestamp: '2021-07-22T13:30:00.000Z', geoState: 'ny', publisher: 'aaa' },
+    { timestamp: '2021-07-22T14:30:00.000Z', geoState: 'ca', publisher: 'bbb' },
+    { timestamp: '2021-07-22T15:30:00.000Z', geoState: 'ca', publisher: 'ccc' }
+  ]
 
-  servertest(server(), url, { encoding: 'json', method: 'POST' }, onRejectResponse)
-    .end(JSON.stringify(visit))
+  var expected = [
+    'http://example-1.com',
+    'http://example-2.com',
+    'http://example-2.com'
+  ]
 
-  function onRejectResponse (err, res) {
-    t.falsy(err, 'no error')
-    t.is(res.statusCode, 200, 'correct statusCode')
-    t.is(res.body.decision, 'reject', 'return reject decision')
+  map(visits, 1, routeTraffic, function (err, decisions) {
+    t.falsy(err, 'should not error')
+    t.deepEqual(decisions, expected, 'decisions should match')
     t.end()
-  }
-})
+  })
 
-test.serial.cb('route returns remaining url', function (t) {
-  const url = '/route'
-  conn.deleteAll()
-  conn.addNew(targetSample)
-  const visit = {
-    geoState: 'ca',
-    publisher: 'abc',
-    timestamp: '2018-07-19T14:28:59.513Z'
-  }
+  function routeTraffic (visit, cb) {
+    var opts = { encoding: 'json', method: 'POST' }
+    var req = servertest(server(), '/route', opts, function (err, res) {
+      if (err) return cb(err)
+      cb(null, res.body.decision)
+    })
 
-  servertest(server(), url, { encoding: 'json', method: 'POST' }, onDecisionResponse)
-    .end(JSON.stringify(visit))
-
-  function onDecisionResponse (err, res) {
-    t.falsy(err, 'no error')
-    t.is(res.statusCode, 200, 'correct statusCode')
-    t.is(res.body.url, targetSample.url, 'return url')
-    t.end()
+    req.end(JSON.stringify(visit))
   }
 })
